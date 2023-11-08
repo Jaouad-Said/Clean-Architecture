bf016de407d9568091ea12684dbdc66a8c76151e
jaouad-said <jaouad_sd@hotmail.com>
2023-11-08 10:24:33 +0100

    Global Error Handling

--- CleaProject.Api/Program.cs
+++ CleaProject.Api/Program.cs
@@ -1,6 +1,8 @@
+using CleanProject.Api.Errors;
 using CleanProject.Api.Filters;
 using CleanProject.Application;
 using CleanProject.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -10,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
     // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, CleanProjectProblemDetailsFactory>();
 }
 
 var app = builder.Build();
